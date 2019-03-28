fb9ae49c623278360d08f2fa394234a280ef276a
austin-stocks <sundeep_chadha@yahoo.com>
2019-03-27 21:27:35 -0500

    Initial version

---
+++ logger_exp.py
@@ -0,0 +1,14 @@
+
+from my_logger import my_print as my_print
+
+
+
+debug_fh = open("logfilename.txt", "w+")
+lvl = "info"
+
+
+debug_str = "1: This is a wonderful world"
+stdout = 0; my_print (debug_fh,debug_str,stdout,lvl)
+
+debug_str = "2: This is a wonderful world"
+stdout = 1; my_print (debug_fh,debug_str,stdout,lvl)
