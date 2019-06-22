d679b9d50389f0c69d47374d787bf05c47d08c83
austin-stocks <sundeep_chadha@yahoo.com>
2019-06-22 09:26:01 -0500

    Starting Experimenting with json

---
+++ Scripts/Experiments/Read_Experiment1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+from pprint import pprint
+
+# data_item = pd.read_json('Read_Experiment1.json')
+# print ("Read Data is", data_item )
+with open('Read_Experiment1.json') as data_file:
+    data_item = json.load(data_file)
+pprint(data_item)
+
+
+print (data_item["maps"][0]["id"])  # will return 'blabla'
+# print(data_item["masks"]["id"])    # will return 'valore'
+print(data_item["om_points"])      # will return 'value'
