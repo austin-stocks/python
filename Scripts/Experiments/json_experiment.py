5b99757f16d32dc7c8dd9b3f3d15f28a0948d183
austin-stocks <sundeep_chadha@yahoo.com>
2019-07-11 19:51:26 -0500

    Some more changes

--- Scripts/Experiments/json_experiment.py
+++ Scripts/Experiments/json_experiment.py
@@ -36,53 +36,53 @@ ticker = "UFPI"
 #
 
 qtr_eps_df = pd.read_csv(ticker + "_earnings.csv")
-
-# Handle the case if the split is not separated by :
-split_dates = list()
-split_multiplier = list()
-if ("Splits" in json_data[ticker]):
-  # if the length of the keys is > 0
-  if (len(json_data[ticker]["Splits"].keys()) > 0 ):
-    split_keys = json_data[ticker]["Splits"].keys()
-    print ("Split Date list is: ", split_keys)
-    for i_key in split_keys:
-      print ("Split Date :", i_key, "Split Factor :", json_data[ticker]["Splits"][i_key])
-      try:
-        split_dates.append(dt.datetime.strptime(str(i_key),"%m/%d/%Y").date())
-      except (ValueError):
-        print ("\n***** Error : The split Date: ",i_key,"does not seem to be right. Should be in the format %m/%d/%Y...please check *****")
-        sys.exit(1)
-      try:
-        (numerator, denominator) = json_data[ticker]["Splits"][i_key].split(":")
-        split_multiplier.append(float(denominator)/float(numerator))
-      except (ValueError):
-        print ("\n***** Error : The split factor: ",json_data[ticker]["Splits"][i_key],"for split date :", i_key , "does not seem to have right format [x:y]...please check *****")
-        sys.exit(1)
-  else:
-    print("\"Splits\" exits but seems empty for ", ticker)
-else:
-  print ("\"Splits\" does not exist for ", ticker)
-
-
-for i in range(len(split_dates)):
-  print ("Split Date: ", split_dates[i], " Split Factor :", split_multiplier[i])
-
-
 qtr_eps_date_list = [dt.datetime.strptime(date, '%m/%d/%Y').date() for date in qtr_eps_df.Date.dropna().tolist()]
 qtr_eps_list = qtr_eps_df.Q_EPS.tolist()
 print ("The date list for qtr_eps is ", qtr_eps_date_list, "\nand the number of elements are", len(qtr_eps_date_list))
 print ("The Earnings list for qtr_eps is ", qtr_eps_list)
 
+# Handle the case if the split is not separated by :
+split_dates = list()
+split_multiplier = list()
+print ("Tickers in json data: ",json_data.keys())
+if (ticker not in json_data.keys()):
+  print (ticker, "does not exist in json data")
+else :
+  if ("Splits" in json_data[ticker]):
+    # if the length of the keys is > 0
+    if (len(json_data[ticker]["Splits"].keys()) > 0 ):
+      split_keys = json_data[ticker]["Splits"].keys()
+      print ("Split Date list is: ", split_keys)
+      for i_key in split_keys:
+        print ("Split Date :", i_key, "Split Factor :", json_data[ticker]["Splits"][i_key])
+        try:
+         split_dates.append(dt.datetime.strptime(str(i_key),"%m/%d/%Y").date())
+        except (ValueError):
+         print ("\n***** Error : The split Date: ",i_key,"does not seem to be right. Should be in the format %m/%d/%Y...please check *****")
+         sys.exit(1)
+        try:
+          (numerator, denominator) = json_data[ticker]["Splits"][i_key].split(":")
+          split_multiplier.append(float(denominator)/float(numerator))
+        except (ValueError):
+          print ("\n***** Error : The split factor: ",json_data[ticker]["Splits"][i_key],"for split date :", i_key , "does not seem to have right format [x:y]...please check *****")
+          sys.exit(1)
+      for i in range(len(split_dates)):
+        qtr_eps_list_mod = qtr_eps_list.copy()
+        print("Split Date :", split_dates[i], " Multiplier : ", split_multiplier[i])
+        for j in range(len(qtr_eps_date_list)):
+          if (split_dates[i] > qtr_eps_date_list[j]):
+            qtr_eps_list_mod[j] = round(qtr_eps_list[j] * split_multiplier[i], 4)
+            print("Earnings date ", qtr_eps_date_list[j], " is older than split date. Changed ", qtr_eps_list[j], " to ",
+                  qtr_eps_list_mod[j])
+        qtr_eps_list = qtr_eps_list_mod.copy()
+    else:
+      print("\"Splits\" exits but seems empty for ", ticker)
+  else:
+    print ("\"Splits\" does not exist for ", ticker)
 
 
-for i in range(len(split_dates)):
-  qtr_eps_list_mod = qtr_eps_list.copy()
-  print ("Split Date :" , split_dates[i] , " Multiplier : ", split_multiplier[i])
-  for j in range(len(qtr_eps_date_list)):
-    if (split_dates[i] > qtr_eps_date_list[j]):
-      qtr_eps_list_mod[j] = round(qtr_eps_list[j]*split_multiplier[i],4)
-      print("Earnings date ", qtr_eps_date_list[j], " is older than split date. Changed ",qtr_eps_list[j], " to ",qtr_eps_list_mod[j] )
-  qtr_eps_list = qtr_eps_list_mod.copy()
+# for i in range(len(split_dates)):
+#   print ("Split Date: ", split_dates[i], " Split Factor :", split_multiplier[i])
 
 qtr_eps_list_mod.clear()
 print ("The date list for qtr_eps is ", qtr_eps_date_list, "\nand the number of elements are", len(qtr_eps_date_list))
