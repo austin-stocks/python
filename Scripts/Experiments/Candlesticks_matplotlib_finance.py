727560c7a21e011ad1caa79c0cafae5bfdede27c
austin-stocks <sundeep_chadha@yahoo.com>
2019-08-02 20:12:49 -0500

    More comments with downloaded stuff from web

--- Scripts/Experiments/Candlesticks_matplotlib_finance.py
+++ Scripts/Experiments/Candlesticks_matplotlib_finance.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from mpl_finance import candlestick_ohlc
-
 import matplotlib
 
 # matplotlib.use('Agg')  # Bypass the need to install Tkinter GUI framework
@@ -52,6 +51,109 @@ plt.savefig('mpl_finance-apple.png')
 plt.show()
 
 '''
+From yet another 
+from matplotlib.finance import *
+
+data = parse_yahoo_historical(fetch_historical_yahoo('CKSW', (2013,1,1), (2013, 6, 1)))
+
+ds, opens, closes, highs, lows, volumes = zip(*data)
+
+# Create figure
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+# Plot the candlestick
+candles = candlestick2(ax1, opens, closes, highs, lows,
+                       width=1, colorup='g')
+
+# Add a seconds axis for the volume overlay
+ax2 = ax1.twinx()
+
+# Plot the volume overlay
+bc = volume_overlay(ax2, opens, closes, volumes, colorup='g', alpha=0.5, width=1)
+ax2.add_collection(bc)
+plt.show()
+'''
+
+'''
+From another answer to share the candles and volume
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+'''
+
+
+'''
+https://www.techtrekking.com/author/pravin/page/11/
+
+################################################################################################
+#	name:	timeseries_OHLC_with_SMA.py
+#	desc:	creates OHLC graph with overlay of simple moving averages
+#	date:	2018-06-15
+#	Author:	conquistadorjd
+################################################################################################
+import pandas as pd
+# import pandas_datareader as datareader
+import matplotlib.pyplot as plt
+import datetime
+from matplotlib.finance import candlestick_ohlc
+# from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
+
+print('*** Program Started ***')
+
+df = pd.read_csv('15-06-2016-TO-14-06-2018HDFCBANKALLN.csv')
+
+# ensuring only equity series is considered
+df = df.loc[df['Series'] == 'EQ']
+
+# Converting date to pandas datetime format
+df['Date'] = pd.to_datetime(df['Date'])
+df["Date"] = df["Date"].apply(mdates.date2num)
+
+# Creating required data in new DataFrame OHLC
+ohlc= df[['Date', 'Open Price', 'High Price', 'Low Price','Close Price']].copy()
+# In case you want to check for shorter timespan
+# ohlc =ohlc.tail(60)
+ohlc['SMA50'] = ohlc["Close Price"].rolling(50).mean()
+
+
+f1, ax = plt.subplots(figsize = (10,5))
+
+# plot the candlesticks
+candlestick_ohlc(ax, ohlc.values, width=.6, colorup='green', colordown='red')
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+
+# Creating SMA columns
+ohlc['SMA5'] = ohlc["Close Price"].rolling(5).mean()
+ohlc['SMA10'] = ohlc["Close Price"].rolling(10).mean()
+ohlc['SMA20'] = ohlc["Close Price"].rolling(20).mean()
+ohlc['SMA50'] = ohlc["Close Price"].rolling(50).mean()
+ohlc['SMA100'] = ohlc["Close Price"].rolling(100).mean()
+ohlc['SMA200'] = ohlc["Close Price"].rolling(200).mean()
+
+#Plotting SMA columns
+# ax.plot(ohlc['Date'], ohlc['SMA5'], color = 'blue', label = 'SMA5')
+# ax.plot(ohlc['Date'], ohlc['SMA10'], color = 'blue', label = 'SMA10')
+# ax.plot(ohlc['Date'], ohlc['SMA20'], color = 'blue', label = 'SMA20')
+ax.plot(ohlc['Date'], ohlc['SMA50'], color = 'green', label = 'SMA50')
+# ax.plot(ohlc.index, df['SMA100'], color = 'blue', label = 'SMA100')
+ax.plot(ohlc['Date'], ohlc['SMA200'], color = 'blue', label = 'SMA200')
+
+# Saving image
+plt.savefig('OHLC with SMA HDFC.png')
+
+# In case you dont want to save image but just displya it
+
+# ohlc.to_csv('ohlc.csv')
+# plt.show()
+print('*** Program ended ***')
+
+
+
+
+
+
+
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
