ec7538fd8371911fc82c7992a16bef2fd0f3238c
austin-stocks <sundeep_chadha@yahoo.com>
2019-10-04 17:30:41 -0500

    More stuff

--- Scripts/Experiments/SC_parse_AAII_Download.py
+++ Scripts/Experiments/SC_parse_AAII_Download.py
@@ -2,6 +2,7 @@
 
 import csv
 import openpyxl
+from openpyxl.styles import PatternFill
 import os
 import xlrd
 import sys
@@ -10,14 +11,125 @@ import pandas as pd
 import datetime as dt
 from yahoofinancials import YahooFinancials
 import time
+import logging
+import xlsxwriter
+
+# ---------------------------------------------------------------------------
+# Set Logging
+# critical, error, warning, info, debug
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='Logging_AAII_Parser.txt',
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+# Disnable and enable global level logging
+logging.disable(sys.maxsize)
+logging.disable(logging.NOTSET)
+# ---------------------------------------------------------------------------
 
 # your code here
-
 dir_path = os.getcwd()
 
+# -----------------------------------------------------------------------------
+# The program write a combination of dataframe and random data
+# -----------------------------------------------------------------------------
+# Create an pandas excel writer based on xlsxwriter.
+writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
+workbook  = writer.book
+
+df1 = pd.DataFrame({'Data': [11, 12, 13, 14]})
+df2 = pd.DataFrame({'Data': [21, 22, 23, 24]})
+df3 = pd.DataFrame({'Data': [31, 32, 33, 34]})
+df4 = pd.DataFrame({'Data': [41, 42, 43, 44]})
+
+df1.to_excel(writer, sheet_name='Sheet1')  # Default position, cell A1.
+df2.to_excel(writer, sheet_name='Sheet1', startcol=3)
+df3.to_excel(writer, sheet_name='Sheet1', startrow=6)
+
+# It is also possible to write the dataframe without the header and index.
+df4.to_excel(writer, sheet_name='Sheet1',
+             startrow=7, startcol=4, header=False, index=False)
+
+worksheet = writer.sheets['Sheet1']
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': True})
+# Add a number format for cells with money.
+money = workbook.add_format({'num_format': '$#,##0'})
+
+# Write some data headers.
+worksheet.write('A1', 'Item', bold)
+worksheet.write('B1', 'Cost', bold)
+
+# Some data we want to write to the worksheet.
+expenses = (
+ ['Rent', 1000],
+ ['Gas',   100],
+ ['Food',  300],
+ ['Gym',    50],
+)
+
+# Start from the first cell below the headers.
+row = 1
+col = 0
+
+# Iterate over the data and write it out row by row.
+for item, cost in (expenses):
+ worksheet.write(row, col,     item)
+ worksheet.write(row, col + 1, cost, money)
+ row += 1
+
+# Write a total using a formula.
+worksheet.write(row, 0, 'Total',       bold)
+worksheet.write(row, 1, '=SUM(B2:B5)', money)
+
+# Access the underlying xlsxwriter worksheet and write to it.
+worksheet.write(0, 0, 'test')
+
+# Apply conditional formatting
+format1 = workbook.add_format({'bg_color':   '#FFC7CE',
+                               'font_color': '#9C0006'})
+worksheet.conditional_format('A1:Z1024', {'type':     'cell',
+                                          'criteria': '>=',
+                                          'value':    50,
+                                          'format':   format1})
+
+writer.save()
+
+# Now save that as an image
+import excel2img
+
+# Save as PNG the range of used cells in test.xlsx on page named "Sheet1"
+# excel2img.export_img("test.xlsx", "test.png", "Sheet1", None)
+
+# Save as BMP the range B2:C15 in test.xlsx on page named "Sheet2"
+excel2img.export_img("test.xlsx", "test.png", "", "Sheet1!A1:J30")
+
+
+print("All Done")
+# -----------------------------------------------------------------------------
+
+
+
+
+# for col_range in range(1, 12):
+#     cell_title = sheet.cell(1, col_range)
+#     cell_title.fill = PatternFill(start_color="8a2be2", end_color="8a2be2", fill_type="solid")
+#
+'''
 # This takes around 141 sec
 start = time.process_time()
-xls = pd.ExcelFile('2019_09_07_AAII_Data.xlsm')
+xls = pd.ExcelFile('2019_04_01_AAII_DATA_Experiment.xlsm')
 print(time.process_time() - start)
 
 # This takes around 11 sec
@@ -40,7 +152,21 @@ aaii_sector_df = pd.read_excel(xls, 'Median-Sectors')
 aaii_industry_df = pd.read_excel(xls, 'Median-Industries')
 print(time.process_time() - start)
 
+
+ticker_list_unclean = aaii_dateandperiod_df['Ticker'].tolist()
+ticker_list = [x for x in ticker_list_unclean if str(x) != 'nan']
+
+for ticker_raw in ticker_list:
+
+  ticker = ticker_raw.replace(" ", "").upper() # Remove all spaces from ticker_raw and convert to uppercase
+  logging.info("========================================================")
+  logging.info("Preparing 0 Ananlysis for " + ticker)
+  logging.info("========================================================")
+
 print ("All Done...")
+'''
+
+
 #
 # # 6/30/2019 - This scripte works as inteneded but get thrown off when there are Yahoo and CNBC projections for the
 # #  same date - an example is AAP - look at date 4/21/2017 this has Yahoo and CNBC data (In my opinion incorrectly
