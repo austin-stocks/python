9b7aeb583a771dcb3da3eb0f49e3ecffaac6d62b
austin-stocks <sundeep_chadha@yahoo.com>
2020-01-28 08:59:34 -0600

    Some cleanup

--- Scripts/SC_CopyLastest_Charts.py
+++ Scripts/SC_CopyLastest_Charts.py
@@ -8,38 +8,32 @@ import re
 import shutil
 
 # # -----------------------------------------------------------------------------
-# # Get all the files in the Earnings directory that end with _earnings.csv
-# # -----------------------------------------------------------------------------
-# for file in os.listdir("C:\Sundeep\Stocks_Automation\Charts"):
-#   if file.endswith(".jpg"):
-#     Chart_fullpath = os.path.join("C:\Sundeep\Stocks_Automation\Charts", file)
-#     # print(file_fullpath)
-#     # -------------------------------------------------------------------------
-#     # Open the file and see if it has a column by the name of <XYN>
-#     # -------------------------------------------------------------------------
-# # -----------------------------------------------------------------------------
-
-# # -----------------------------------------------------------------------------
 # # Read the master tracklist file into a dataframe
 # # -----------------------------------------------------------------------------
 dir_path = os.getcwd()
 user_dir = "\\..\\" + "User_Files"
+charts_dir = "\\..\\" + "Charts"
+charts_latest_dir = "\\..\\" + "Latest_Charts"
+
 master_tracklist_file = "Master_Tracklist.xlsx"
 master_tracklist_df = pd.read_excel(dir_path + user_dir + "\\" + master_tracklist_file, sheet_name="Main")
 master_tracklist_df.sort_values('Ticker', inplace=True)
 ticker_list_unclean = master_tracklist_df['Ticker'].tolist()
 ticker_list = [x for x in ticker_list_unclean if str(x) != 'nan']
 master_tracklist_df.set_index('Ticker', inplace=True)
-# # -----------------------------------------------------------------------------
-#
-# Get the Charts files in a list
-all_chart_files_list=os.listdir(r"C:\\Sundeep\\Stocks_Automation\\Charts\\")
-print ("The files in the chart direcotry are", all_chart_files_list)
+# -----------------------------------------------------------------------------
 
+# -----------------------------------------------------------------------------
+# Get the files from the chart directory in a list
+# and remove all the files from the Charts Latest directory
+# -----------------------------------------------------------------------------
+all_chart_files_list=os.listdir(dir_path + charts_dir + "\\")
+print ("The files in the chart direcotry are", all_chart_files_list)
+list( map( os.unlink, (os.path.join(dir_path + charts_latest_dir + "\\" ,f) for f in os.listdir(dir_path + charts_latest_dir + "\\")) ) )
+# -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
 # Loop through all the wheat tickers in the master Tracklist file
-# And find the corresponding jpg files from the charts directory
 # -----------------------------------------------------------------------------
 for ticker_raw in ticker_list:
   ticker = ticker_raw.replace(" ", "").upper() # Remove all spaces from ticker_raw and convert to uppercase
@@ -53,10 +47,15 @@ for ticker_raw in ticker_list:
     print (ticker , " is not Wheat...skipping")
     continue
 
+  # Find the corresponding jpg files from the charts directory list
+  # todo : Get the regular expression so that it matches the numbers...so that we don't get
+  # any spurious (like AMZN_2019_12_01_thislooksgood.jpg or AMZN_thislooksbac.jpg)
   my_regex = re.compile(re.escape(ticker) + re.escape("_")  + ".*jpg")
   ticker_chart_files_list = list(filter(my_regex.match, all_chart_files_list)) # Read Note
   print(ticker_chart_files_list)
 
+  # For each file split the file name to get the date string.
+  # Then covert the date string to datetime and append it to the list
   ticker_chart_date_list = []
   for ticker_chart_filename in ticker_chart_files_list:
     # Check the length of the filename - it should be number of characters in the ticker and 16
@@ -70,8 +69,9 @@ for ticker_raw in ticker_list:
     print ("The date string for ", ticker_chart_filename , "is ", ticker_chart_date_str, "and the datetime is ", ticker_chart_date_dt)
     ticker_chart_date_list.append(ticker_chart_date_dt)
 
-
   print ("The datetime list for ", ticker, " is ", ticker_chart_date_list)
+  # Sort the list to the get the latest (youngest) datetime
+  # and create a string for the ticker filename from the string
   ticker_chart_date_list.sort(reverse=True)
   print ("The datetime SORTED list for ", ticker, " is ", ticker_chart_date_list)
   if (len(ticker_chart_date_list) > 0):
@@ -79,4 +79,6 @@ for ticker_raw in ticker_list:
     ticker_latest_chart_filename = ticker + "_" + ticker_latest_chart + ".jpg"
     print ("The latest file for ", ticker, "is", ticker_latest_chart_filename)
 
-  shutil.copy2('C:\\Sundeep\\Stocks_Automation\\Charts\\' + ticker_latest_chart_filename, 'C:\\Sundeep\\Stocks_Automation\\Latest_Charts')
+  # Copy the chart file - that was the youngest - to the desitnation directory as ticker_Latest.jpg
+  shutil.copy2(dir_path + charts_dir + "\\" + ticker_latest_chart_filename, dir_path + charts_latest_dir + "\\" + ticker + "_Latest.jpg")
+
