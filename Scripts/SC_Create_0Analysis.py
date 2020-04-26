930eb738eaab60523ef5884ba0efa710cd728cc7
austin-stocks <sundeep_chadha@yahoo.com>
2020-04-26 13:54:42 -0500

    More updates...

--- Scripts/SC_Create_0Analysis.py
+++ Scripts/SC_Create_0Analysis.py
@@ -75,7 +75,7 @@ ticker_list = [x for x in ticker_list_unclean if str(x) != 'nan']
 # #############################################################################
 #                   MAIN LOOP FOR TICKERS
 # #############################################################################
-ticker_list = ['NATI']
+ticker_list = ['JPM']
 for ticker_raw in ticker_list:
 
   ticker = ticker_raw.replace(" ", "").upper()  # Remove all spaces from ticker_raw and convert to uppercase
@@ -93,7 +93,7 @@ for ticker_raw in ticker_list:
   logging.debug("The YR datain df \n" + ticker_datain_yr_df.to_string())
 
   # Remove the rows that are not needed, if that is ever needed
-  ticker_datain_yr_df.drop(index= ['XYZ'], inplace=True)
+  # ticker_datain_yr_df.drop(index= ['XYZ'], inplace=True)
   logging.debug("The YR datain df are removing the unwanted rows \n" + ticker_datain_yr_df.to_string())
 
   # ---------------------------------------------------------------------------
@@ -114,7 +114,7 @@ for ticker_raw in ticker_list:
 
 
   # ---------------------------------------------------------------------------
-  ticker_yr_lynch_df.applymap(lambda x: str(int(x)) if abs(x - int(x)) < 1e-6 else str(round(x, 2)))
+  # ticker_yr_lynch_df.applymap(lambda x: str(int(x)) if abs(x - int(x)) < 1e-6 else str(round(x, 2)))
 
   fig = plt.figure()
   fig.set_size_inches(14.431, 7.639)  # Length x height
@@ -123,8 +123,13 @@ for ticker_raw in ticker_list:
   table1_plt = plt.subplot2grid((5, 6), (0, 5), colspan=1, rowspan=4)
   table2_plt = plt.subplot2grid((5, 6), (4, 0), colspan=6, rowspan=2)
   plt.subplots_adjust(hspace=0, wspace=0)
-
-
+  fig.suptitle("Analysis for " + ticker)
+  main_plt.title.set_text("Maybe some Chart")
+  table1_plt.title.set_text("Key Numbers")
+  table2_plt.title.set_text("Yearly Table")
+  main_plt.set_facecolor("lightgrey")
+  table1_plt.set_facecolor("lightgrey")
+  table2_plt.set_facecolor("lightgrey")
 
   main_plt.plot([1, 2, 3])
 
@@ -136,17 +141,82 @@ for ticker_raw in ticker_list:
   table2_plt.set_yticks([])
   table2_plt.set_xticks([])
 
-  table1_plt_inst = table1_plt.table(cellText=[[1, 1], [2, 2]], rowLabels=['row1', 'row2'], colLabels=['col1', 'col2'])
+  table1_plt_inst = table1_plt.table(cellText=[[1,1,1], [2,2,2]], rowLabels=['row1', 'row2'], colLabels=['col1', 'col2','col3'])
   # table2_plt.table(cellText=[[3, 3], [4, 4]], loc='upper center',rowLabels=['row1', 'row2'], colLabels=['col1', 'col2'])
+  table1_plt_inst[(1,0)].set_facecolor("#56b5fd")
+
   table1_plt.axis('off')
 
 
+  # This reverses the dataframe by columns - Both of these work
+  # tmp_df = ticker_yr_lynch_df[ticker_yr_lynch_df.columns[::-1]]
+  tmp_df = ticker_yr_lynch_df.iloc[:, ::-1]
+  ticker_yr_lynch_df = tmp_df.copy()
+  logging.debug("The dataframe now is \n" + ticker_yr_lynch_df.to_string())
   table2_plt_inst = table2_plt.table(cellText=ticker_yr_lynch_df.values, rowLabels=ticker_yr_lynch_df.index,colWidths=[0.1] * len(ticker_yr_lynch_df.columns),colLabels=ticker_yr_lynch_df.columns)
-            # cellLoc='center', rowLoc='center',
-            # loc='bottom')
+
+  # for tmp_val in ticker_yr_lynch_df.values:
+  #   logging.debug("The value is " + str(tmp_val))
+  #   for tmp_tmp_val in tmp_val:
+  #     logging.debug("The inner val is " + str(tmp_tmp_val))
+      # if (tmp_tmp_val < 0):
+      #   table1_plt[]
+
+  # Sundeep is here
+  # Find out how to deal with NA
+  # Figure out how to limit the width of the columns
+  # Font of the value?
+  logging.debug("")
+  logging.debug("Will now set the appropriate cell colors in the dataframe")
+
+  current_assets_row_idx =  ticker_yr_lynch_df.index.get_loc('Current_Assets') +1
+  logging.debug("The row_idx corresponding to index Current_Assets is " + str(current_assets_row_idx))
+  # The column header starts with (0, 0)...(0, n - 1).The row header starts with (1, -1)...(n, -1)
+  for key, cell in table2_plt_inst.get_celld().items():
+    row_idx = key[0]
+    col_idx = key[1]
+    cell_val = cell.get_text().get_text()
+    logging.debug("Row idx " + str(row_idx) + " Col idx " + str(col_idx) + " Value " + str(cell_val))
+    if ((row_idx == 0) or (col_idx < 0)):
+      continue
+    elif (cell_val == 'nan'):
+      x = "-"
+      cell.get_text().set_text(x)
+    else:
+      if (row_idx == current_assets_row_idx):
+        x = f'{int(float(cell_val)):,}'
+      else:
+        x =  f'{float(cell.get_text().get_text()):.2f}'
+      cell.get_text().set_text(x)
+      if float(cell_val) < 0:
+        cell.get_text().set_color('Red')
+        cell.get_text().set_fontstyle('italic')
+        # cell.set_color('lightgrey')
+
+  # table_props = table2_plt_inst.properties()
+  # logging.debug("The dictionary of table properties is \n" + str(table_props))
+  # table_cells = table_props['child_artists']
+  # for cell in table_cells:
+  #   cell_val = cell.get_text()
+  #   logging.debug("The cell is " + str(cell) + " and the value is " + str(cell_value) + " and the color is " + str(cell_val.get_color()))
+  #   # cell_val.set_color('green')
+  #   cell_val.set_text("HAHA")
+  #   logging.debug("The cell is " + str(cell) + " and the value is " + str(cell_val) + " and the color is " + str(cell_val.get_color()))
+  #   # logging.debug("Now the color is " + str(cell_val.get_color()))
+
+
+  # for row_idx in range(len(ticker_yr_lynch_df.values)):
+  #   logging.debug("\nThe row idx is " + str(row_idx) + " and the row value is " + str(ticker_yr_lynch_df.index[row_idx]))
+  #   row_val_list = ticker_yr_lynch_df.iloc[row_idx]
+  #   logging.debug("The columns corresponding to the row idx " + str(row_idx) + " are\n" + str(row_val_list))
+  #   for col_idx in range(len(row_val_list)):
+  #     logging.debug("The col idx is " + str(col_idx) + " and the value is " + str(row_val_list[col_idx]))
+  #     if (row_val_list[col_idx] < 0):
+  #       table2_plt_inst[(row_idx+1, col_idx)].set_facecolor('grey')
+
   table2_plt.axis('off')
-  table2_plt_inst.auto_set_font_size(False)
-  table2_plt_inst.set_fontsize(12)
+  # table2_plt_inst.auto_set_font_size(False)
+  # table2_plt_inst.set_fontsize(12)
 
   plt.show()
 
