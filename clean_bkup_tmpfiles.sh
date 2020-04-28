46b5a288c53f2028ac8a68143fad53f135c12321
austin-stocks <sundeep_chadha@yahoo.com>
2020-04-27 20:43:33 -0500

    More updates

--- clean_bkup_tmpfiles.sh
+++
@@ -1,7 +0,0 @@
-echo "Cleaning up the backup files that are appreantly created by MyCloud backup"
-
-rm -f *-2020-04-*.csv
-rm -f Dividend/*-2020-04-*.csv
-rm -f Earnings/*-2020-04-*.csv
-rm -f User_Files/*-2020-04-*.csv
-rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-04-*.jpg
