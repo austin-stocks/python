c7a5e5be8ff0737a5a2804b03e0966b831c3227a
austin-stocks <sundeep_chadha@yahoo.com>
2020-07-12 19:41:36 -0500

    More updates

--- cleanup_tmpfiles.sh
+++ cleanup_tmpfiles.sh
@@ -5,4 +5,19 @@ rm -f historical/*-2020-07-*.csv
 rm -f Dividend/*-2020-07-*.csv
 rm -f Earnings/*-2020-07-*.csv
 rm -f User_Files/*-2020-07-*.csv
+
+rm -f Charts/Linear/Charts_With_Numbers/*-2020-07-*.jpg
+rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-07-*.jpg
+rm -f Charts/Log/Charts_With_Numbers/*-2020-07-*.jpg
+
+rm -f Charts/Linear/Charts_Without_Numbers/*-2020-07-*.jpg
+rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-07-*.jpg
+rm -f Charts/Log/Charts_Without_Numbers/*-2020-07-*.jpg
+
 rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-07-*.jpg
+rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-07-*.jpg
+rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-07-*.jpg
+
+rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-07-*.jpg
+rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-07-*.jpg
+rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-07-*.jpg
