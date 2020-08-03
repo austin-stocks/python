6935021d8f30ddc2ae153bd35f11fceef1e468c6
austin-stocks <sundeep_chadha@yahoo.com>
2020-08-03 16:26:33 -0500

    More updates

--- cleanup_tmpfiles.sh
+++ cleanup_tmpfiles.sh
@@ -1,25 +1,27 @@
 echo "Cleaning up the backup files that are appreantly created by MyCloud backup"
 
-rm -f *-2020-07-*.csv
-rm -f AAII_Financials/Quarterly/*-2020-07-*.xlsx
-rm -f Download/YahooHistorical/*-2020-07-*.csv
-rm -f historical/*-2020-07-*.csv
-rm -f Dividend/*-2020-07-*.csv
-rm -f Earnings/*-2020-07-*.csv
-rm -f User_Files/*-2020-07-*.csv
+rm -f *-2020-08-*.csv
+rm -f AAII_Financials/Quarterly/*-2020-08-*.xlsx
+rm -f AAII_Financials/Yearly/*-2020-08-*.xlsx
 
-rm -f Charts/Linear/Charts_With_Numbers/*-2020-07-*.jpg
-rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-07-*.jpg
-rm -f Charts/Log/Charts_With_Numbers/*-2020-07-*.jpg
+rm -f Download/YahooHistorical/*-2020-08-*.csv
+rm -f historical/*-2020-08-*.csv
+rm -f Dividend/*-2020-08-*.csv
+rm -f Earnings/*-2020-08-*.csv
+rm -f User_Files/*-2020-08-*.csv
 
-rm -f Charts/Linear/Charts_Without_Numbers/*-2020-07-*.jpg
-rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-07-*.jpg
-rm -f Charts/Log/Charts_Without_Numbers/*-2020-07-*.jpg
+rm -f Charts/Linear/Charts_With_Numbers/*-2020-08-*.jpg
+rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-08-*.jpg
+rm -f Charts/Log/Charts_With_Numbers/*-2020-08-*.jpg
 
-rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-07-*.jpg
-rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-07-*.jpg
-rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-07-*.jpg
+rm -f Charts/Linear/Charts_Without_Numbers/*-2020-08-*.jpg
+rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-08-*.jpg
+rm -f Charts/Log/Charts_Without_Numbers/*-2020-08-*.jpg
 
-rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-07-*.jpg
-rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-07-*.jpg
-rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-07-*.jpg
+rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-08-*.jpg
+rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-08-*.jpg
+rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-08-*.jpg
+
+rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-08-*.jpg
+rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-08-*.jpg
+rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-08-*.jpg
