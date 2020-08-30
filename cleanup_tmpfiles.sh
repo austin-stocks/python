83bcadeee92c40e2b3f6721a408542427abd9907
austin-stocks <sundeep_chadha@yahoo.com>
2020-08-30 12:47:31 -0500

    More updates

--- cleanup_tmpfiles.sh
+++ cleanup_tmpfiles.sh
@@ -1,14 +1,12 @@
 echo "Cleaning up the backup files that are appreantly created by MyCloud backup"
 
 rm -f *-2020-08-*.csv
+
 rm -f AAII_Financials/Quarterly/*-2020-08-*.xlsx
 rm -f AAII_Financials/Yearly/*-2020-08-*.xlsx
 
-rm -f Download/YahooHistorical/*-2020-08-*.csv
-rm -f historical/*-2020-08-*.csv
-rm -f Dividend/*-2020-08-*.csv
-rm -f Earnings/*-2020-08-*.csv
-rm -f User_Files/*-2020-08-*.csv
+rm -f Analysis/Quarterly/*-2020-08-*.xlsx
+rm -f Analysis/Yearly/*-2020-08-*.xlsx
 
 rm -f Charts/Linear/Charts_With_Numbers/*-2020-08-*.jpg
 rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-08-*.jpg
@@ -18,6 +16,19 @@ rm -f Charts/Linear/Charts_Without_Numbers/*-2020-08-*.jpg
 rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-08-*.jpg
 rm -f Charts/Log/Charts_Without_Numbers/*-2020-08-*.jpg
 
+rm -f Dividend/*-2020-08-*.csv
+
+rm -f Download/YahooHistorical/*-2020-08-*.csv
+
+rm -f Downloaded_from_AAII/*/Analysis/*-2020-08-*.xlsm
+rm -f Downloaded_from_AAII/*/Financials - Quarterly/*-2020-08-*.xlsm
+rm -f Downloaded_from_AAII/*/Financials - Yearly/*-2020-08-*.xlsm
+rm -f Downloaded_from_AAII/*/Key Statistics/*-2020-08-*.xlsm
+
+rm -f Earnings/*-2020-08-*.csv
+
+rm -f historical/*-2020-08-*.csv
+
 rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-08-*.jpg
 rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-08-*.jpg
 rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-08-*.jpg
@@ -25,3 +36,11 @@ rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-08-*.jpg
 rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-08-*.jpg
 rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-08-*.jpg
 rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-08-*.jpg
+
+rm -f Logs/*-2020-08-*.csv
+
+rm -f Readme/*-2020-08-*.csv
+
+rm -f User_Files/*-2020-08-*.csv
+
+
