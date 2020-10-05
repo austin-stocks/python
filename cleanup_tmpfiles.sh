9303d745bf4b45574b1ead726d41b4e691beb37d
austin-stocks <sundeep_chadha@yahoo.com>
2020-10-05 17:41:55 -0500

    More updates

--- cleanup_tmpfiles.sh
+++ cleanup_tmpfiles.sh
@@ -1,46 +1,46 @@
 echo "Cleaning up the backup files that are appreantly created by MyCloud backup"
 
-rm -f *-2020-09-*.csv
+rm -f *-2020-10-*.csv
 
-rm -f AAII_Financials/Quarterly/*-2020-09-*.xlsx
-rm -f AAII_Financials/Yearly/*-2020-09-*.xlsx
+rm -f AAII_Financials/Quarterly/*-2020-10-*.xlsx
+rm -f AAII_Financials/Yearly/*-2020-10-*.xlsx
 
-rm -f Analysis/Quarterly/*-2020-09-*.xlsx
-rm -f Analysis/Yearly/*-2020-09-*.xlsx
+rm -f Analysis/Quarterly/*-2020-10-*.xlsx
+rm -f Analysis/Yearly/*-2020-10-*.xlsx
 
-rm -f Charts/Linear/Charts_With_Numbers/*-2020-09-*.jpg
-rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-09-*.jpg
-rm -f Charts/Log/Charts_With_Numbers/*-2020-09-*.jpg
+rm -f Charts/Linear/Charts_With_Numbers/*-2020-10-*.jpg
+rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-10-*.jpg
+rm -f Charts/Log/Charts_With_Numbers/*-2020-10-*.jpg
 
-rm -f Charts/Linear/Charts_Without_Numbers/*-2020-09-*.jpg
-rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-09-*.jpg
-rm -f Charts/Log/Charts_Without_Numbers/*-2020-09-*.jpg
+rm -f Charts/Linear/Charts_Without_Numbers/*-2020-10-*.jpg
+rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-10-*.jpg
+rm -f Charts/Log/Charts_Without_Numbers/*-2020-10-*.jpg
 
-rm -f Dividend/*-2020-09-*.csv
+rm -f Dividend/*-2020-10-*.csv
 
-rm -f Download/YahooHistorical/*-2020-09-*.csv
+rm -f Download/YahooHistorical/*-2020-10-*.csv
 
-rm -f Downloaded_from_AAII/*/Analysis/*-2020-09-*.xlsm
-rm -f Downloaded_from_AAII/*/Financials - Quarterly/*-2020-09-*.xlsm
-rm -f Downloaded_from_AAII/*/Financials - Yearly/*-2020-09-*.xlsm
-rm -f Downloaded_from_AAII/*/Key Statistics/*-2020-09-*.xlsm
+rm -f Downloaded_from_AAII/*/Analysis/*-2020-10-*.xlsm
+rm -f Downloaded_from_AAII/*/Financials - Quarterly/*-2020-10-*.xlsm
+rm -f Downloaded_from_AAII/*/Financials - Yearly/*-2020-10-*.xlsm
+rm -f Downloaded_from_AAII/*/Key Statistics/*-2020-10-*.xlsm
 
-rm -f Earnings/*-2020-09-*.csv
+rm -f Earnings/*-2020-10-*.csv
 
-rm -f historical/*-2020-09-*.csv
+rm -f historical/*-2020-10-*.csv
 
-rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-09-*.jpg
-rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-09-*.jpg
-rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-09-*.jpg
+rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-10-*.jpg
+rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-10-*.jpg
+rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-10-*.jpg
 
-rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-09-*.jpg
-rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-09-*.jpg
-rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-09-*.jpg
+rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-10-*.jpg
+rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-10-*.jpg
+rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-10-*.jpg
 
-rm -f Logs/*-2020-09-*.csv
+rm -f Logs/*-2020-10-*.csv
 
-rm -f Readme/*-2020-09-*.csv
+rm -f Readme/*-2020-10-*.csv
 
-rm -f User_Files/*-2020-09-*.csv
+rm -f User_Files/*-2020-10-*.csv
 
 
