5d4f4e55a35de15e3f31813b1e7594d586ff01ce
austin-stocks <sundeep_chadha@yahoo.com>
2020-10-30 15:49:30 -0500

    More updates

--- cleanup_tmpfiles.sh
+++ cleanup_tmpfiles.sh
@@ -1,55 +1,62 @@
 echo "Cleaning up the backup files that are appreantly created by MyCloud backup"
 
-rm -f *-2020-10-*.csv
 
-rm -f AAII_Financials/Quarterly/*-2020-10-*.xlsx
-rm -f AAII_Financials/Yearly/*-2020-10-*.xlsx
+my_dates='-2020-09-  -2020-10- -2020-11-'
 
-rm -f Analysis/Quarterly/*-2020-10-*.csv
-rm -f Analysis/Yearly/*-2020-10-*.csv
-rm -f Analysis/Key_statistics/*-2020-10-*.csv
+for tmp_date in $my_dates
+do
+  echo "Removing files that have $tmp_date string"
 
-rm -f Analysis_Plots/*-2020-10-*.jpg
-rm -f Analysis_Watchlist/Wheat/*-2020-10-*.jpg
-rm -f Analysis_Watchlist/Not_Wheat/*-2020-10-*.jpg
-
-
-rm -f Charts/Linear/Charts_With_Numbers/*-2020-10-*.jpg
-rm -f Charts/Long_Linear/Charts_With_Numbers/*-2020-10-*.jpg
-rm -f Charts/Log/Charts_With_Numbers/*-2020-10-*.jpg
-
-rm -f Charts/Linear/Charts_Without_Numbers/*-2020-10-*.jpg
-rm -f Charts/Long_Linear/Charts_Without_Numbers/*-2020-10-*.jpg
-rm -f Charts/Log/Charts_Without_Numbers/*-2020-10-*.jpg
-
-rm -f Dividend/*-2020-10-*.csv
-
-rm -f Download/YahooHistorical/*-2020-10-*.csv
-
-rm -f Downloaded_from_AAII/*/Analysis/*-2020-10-*.xlsm
-rm -f Downloaded_from_AAII/*/Financials - Quarterly/*-2020-10-*.xlsm
-rm -f Downloaded_from_AAII/*/Financials - Yearly/*-2020-10-*.xlsm
-rm -f Downloaded_from_AAII/*/Key Statistics/*-2020-10-*.xlsm
-
-rm -f Earnings/*-2020-10-*.csv
-
-rm -f historical/*-2020-10-*.csv
-
-rm -f Latest_Charts/Linear/Charts_With_Numbers/*-2020-10-*.jpg
-rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*-2020-10-*.jpg
-rm -f Latest_Charts/Log/Charts_With_Numbers/*-2020-10-*.jpg
-
-rm -f Latest_Charts/Linear/Charts_Without_Numbers/*-2020-10-*.jpg
-rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*-2020-10-*.jpg
-rm -f Latest_Charts/Log/Charts_Without_Numbers/*-2020-10-*.jpg
-
-rm -f Logs/*-2020-10-*.csv
-rm -f Logs/*-2020-10-*.txt
-
-rm -f Readme/*-2020-10-*.csv
-rm -f Readme/*-2020-10-*.txt
-
-rm -f User_Files/*-2020-10-*.csv
-rm -f User_Files/*-2020-10-*.txt
-
-
+   rm -f AAII_Financials/Quarterly/*$tmp_date*.xlsx
+   rm -f AAII_Financials/Yearly/*$tmp_date*.xlsx
+ 
+   rm -f Analysis/Quarterly/*$tmp_date*.csv
+   rm -f Analysis/Yearly/*$tmp_date*.csv
+   rm -f Analysis/Key_statistics/*$tmp_date*.csv
+ 
+   rm -f Analysis_Plots/*$tmp_date*.jpg
+   rm -f Analysis_Watchlist/Wheat/*$tmp_date*.jpg
+   rm -f Analysis_Watchlist/Not_Wheat/*$tmp_date*.jpg
+ 
+ 
+   rm -f Charts/Linear/Charts_With_Numbers/*$tmp_date*.jpg
+   rm -f Charts/Long_Linear/Charts_With_Numbers/*$tmp_date*.jpg
+   rm -f Charts/Log/Charts_With_Numbers/*$tmp_date*.jpg
+ 
+   rm -f Charts/Linear/Charts_Without_Numbers/*$tmp_date*.jpg
+   rm -f Charts/Long_Linear/Charts_Without_Numbers/*$tmp_date*.jpg
+   rm -f Charts/Log/Charts_Without_Numbers/*$tmp_date*.jpg
+ 
+   rm -f Dividend/*$tmp_date*.csv
+ 
+   rm -f Download/YahooHistorical/*$tmp_date*.csv
+ 
+   rm -f Downloaded_from_AAII/*/Analysis/*$tmp_date*.xlsm
+   rm -f Downloaded_from_AAII/*/"Financials - Quarterly"/*$tmp_date*.xlsm
+   rm -f Downloaded_from_AAII/*/"Financials - Yearly"/*$tmp_date*.xlsm
+   rm -f Downloaded_from_AAII/*/"Key Statistics"/*$tmp_date*.xlsm
+ 
+   rm -f Earnings/*$tmp_date*.csv
+ 
+   rm -f historical/*$tmp_date*.csv
+ 
+   rm -f Latest_Charts/Linear/Charts_With_Numbers/*$tmp_date*.jpg
+   rm -f Latest_Charts/Long_Linear/Charts_With_Numbers/*$tmp_date*.jpg
+   rm -f Latest_Charts/Log/Charts_With_Numbers/*$tmp_date*.jpg
+ 
+   rm -f Latest_Charts/Linear/Charts_Without_Numbers/*$tmp_date*.jpg
+   rm -f Latest_Charts/Long_Linear/Charts_Without_Numbers/*$tmp_date*.jpg
+   rm -f Latest_Charts/Log/Charts_Without_Numbers/*$tmp_date*.jpg
+ 
+   rm -f Logs/*$tmp_date*.csv
+   rm -f Logs/*$tmp_date*.txt
+ 
+   rm -f Readme/*$tmp_date*.csv
+   rm -f Readme/*$tmp_date*.txt
+ 
+   rm -f User_Files/*$tmp_date*.csv
+   rm -f User_Files/*$tmp_date*.txt
+ 
+ done
+ 
+ 
