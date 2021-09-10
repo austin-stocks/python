86538c9c9465a0808726bb35a2e06054267df3bc
Sundeep Chadha <sundeep_chadha@yahoo.com>
2021-09-09 20:38:08 -0500

    More changes

--- Analysis/git_commit.sh
+++ Analysis/git_commit.sh
@@ -8,14 +8,27 @@ echo "=====> Now starting the add process <====="
 echo ""
 
 alphabets='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+alphabets_subloop='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
 
 
 for chrs in $alphabets 
 do 
   echo "Adding the files that start with [$chrs]*"
-  git add Quarterly/[$chrs]*.csv
-  git add Yearly/[$chrs]*.csv 
-  git add Key_Statistics/[$chrs]*.csv
+  if [ $chrs = "A" ]
+  then
+    echo "Doing $chrs...Will loop through subloop"
+    for chrs_subloop in $alphabets_subloop
+    do
+      echo "Adding the files that start with ==> [$chrs][$chrs_subloop]"
+      git  add Quarterly/[$chrs][$chrs_subloop]*csv -v
+      git  add Yearly/[$chrs][$chrs_subloop]*csv -v
+      git  add Key_Statistics/[$chrs][$chrs_subloop]*csv -v
+    done
+  else
+    git add Quarterly/[$chrs]*.csv
+    git add Yearly/[$chrs]*.csv 
+    git add Key_Statistics/[$chrs]*.csv
+  fi 
 done
 
 
@@ -27,8 +40,20 @@ do
   echo "Commiting the files that start with [$chrs]*"
   echo ""
   sleep 2
-  git commit -m "More Updates"  Quarterly/[$chrs]*.csv -v 
-  git commit -m "More Updates"  Yearly/[$chrs]*.csv -v 
-  git commit -m "More Updates"  Key_Statistics/[$chrs]*.csv -v 
+  if [ $chrs = "A" ]
+  then
+    echo "Doing $chrs...Will loop through subloop"
+    for chrs_subloop in $alphabets_subloop
+    do
+      echo "Committing the files that start with ==> [$chrs][$chrs_subloop]"
+      git commit -m "More Updates" Quarterly/[$chrs][$chrs_subloop]*csv -v
+      git commit -m "More Updates" Yearly/[$chrs][$chrs_subloop]*csv -v
+      git commit -m "More Updates" Key_Statistics/[$chrs][$chrs_subloop]*csv -v
+    done
+  else
+    git commit -m "More Updates"  Quarterly/[$chrs]*.csv -v 
+    git commit -m "More Updates"  Yearly/[$chrs]*.csv -v 
+    git commit -m "More Updates"  Key_Statistics/[$chrs]*.csv -v 
+  fi 
 done
 
