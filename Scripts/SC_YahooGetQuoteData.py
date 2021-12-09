924b2e871e4f17a6270c6249f8f3c667447f1257
Sundeep Chadha <sundeep_chadha@yahoo.com>
2021-12-08 19:56:45 -0600

    More changes

--- Scripts/SC_YahooGetQuoteData.py
+++ Scripts/SC_YahooGetQuoteData.py
@@ -11,18 +11,35 @@ print ("Here I am")
 #  more details about it are available at 
 # http://theautomatic.net/yahoo_fin-documentation/#python_version
 
-ticker = "gm"
-ticker_quote_dict = get_quote_table(ticker)
-ticker_holders_dict = get_holders(ticker)
+ticker_list=["AAON","ACU","ADUS","AME","AMP","ASGN","AUDC","AVTR","AYI","BAH","BLD","BRO","CACC","CACI","CASH","CASY","CBOE","CCK","CCMP","CCOI","CHD","CHE","CHRW","CMBM","CNXN","CPSI","CSGP","CSGS","CTAS","DCO","ECOM","EME","ENSG","ESNT","FAF","FDS","FHI","FIX","FN","FORM","FOXA","FOXF","G","GDOT","GLDD","GOOG","HCSG","HEI","HELE","HONE","HSIC","HTH","IAA","IBP","ICLR","IDCC","INOV","IOSP","ISBC","JBSS","JBT","KAI","KEX","LFUS","LGIH","LHCG","LOPE","LPLA","LSI","LSTR","MANT","MBUU","MEDP","MLKN","MMS","MRCY","MSEX","MTX","MYRG","NMIH","NSIT","NSP","NSSC","NTGR","NXST","OMCL","OSIS","OTEX","PFSI","PNTG","POWL","PRAA","PRI","RJF","SAIA","SCPL","SEM","SGC","SITE","SLGN","SMPL","SNX","SSD","SSNC","TCBI","TDY","TFX","TKR","TNET","TRU","TSM","TTGT","TXRH","TYL","UFPI","UFPT","UNF","USPH","VRSK","WAL","WLDN","WLTW","WNS"]
+ticker_list=["FIZZ", "RH", "THO", "AVGO", "CIEN", "COST", "LULU", "ORCL"]
 
-print ("Ticker Quote Dict is", ticker_quote_dict)
-print ("Ticker Holders  Dict is", ticker_holders_dict)
+# i_int = 0
+# for ticker_raw in ticker_list:
+#   ticker = ticker_raw.replace(" ", "").upper() # Remove all spaces from ticker_raw and convert to uppercase
+#   i_int += 1
+#   ticker_quote_dict = get_quote_table(ticker)
+#   ticker_earnings_date = ticker_quote_dict['Earnings Date']
+#
+#   print ("Next earnings date for : ", ticker ," is : ", ticker_earnings_date)
 
-ticker_1y_target_price  = ticker_quote_dict['1y Target Est']
-ticker_earnings_date = ticker_quote_dict['Earnings Date']
-ticker_curr_price = ticker_quote_dict['Quote Price']
-print ("Current Price : ", ticker_curr_price, ", 1 yr Target price is : ", ticker_1y_target_price, ", Upside : ", (ticker_1y_target_price/ticker_curr_price -1)*100, "%")
-print ("Next earnings date is : ", ticker_earnings_date)
+earnings_in_week_dict_list = get_earnings_in_date_range('12/12/2021', '12/19/2021')
+print ("The earning in the week are ", earnings_in_week_dict_list)
+for tmp_dict in earnings_in_week_dict_list:
+    print ("Ticker : ", tmp_dict['ticker'], ", Earnings Date : " ,tmp_dict['startdatetime'])
+
+# ticker = "gm"
+# ticker_quote_dict = get_quote_table(ticker)
+# ticker_holders_dict = get_holders(ticker)
+#
+# print ("Ticker Quote Dict is", ticker_quote_dict)
+# print ("Ticker Holders  Dict is", ticker_holders_dict)
+#
+# ticker_1y_target_price  = ticker_quote_dict['1y Target Est']
+# ticker_earnings_date = ticker_quote_dict['Earnings Date']
+# ticker_curr_price = ticker_quote_dict['Quote Price']
+# print ("Current Price : ", ticker_curr_price, ", 1 yr Target price is : ", ticker_1y_target_price, ", Upside : ", (ticker_1y_target_price/ticker_curr_price -1)*100, "%")
+# print ("Next earnings date is : ", ticker_earnings_date)
 
 
 # The following methods are supported by the module stock_info
