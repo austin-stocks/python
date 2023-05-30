cc33982ed29350110b9764fb061608fc7f1ac4c0
Sundeep Chadha <sundeep_chadha@yahoo.com>
2023-05-30 10:55:27 -0500

    More Updates

---
+++ Scripts/SC_IBHistorical_Download.py
@@ -0,0 +1,75 @@
+'''
+The ib.reqHistoricalData() works back in time.
+It starts at the end date and goes back untile it runs out of bars
+or in my case I set a start date so it quits when it gets there.
+I don't know why the duration doesn't seem to do what I anticipated.
+'''
+
+# Based on: https://ib-insync.readthedocs.io/recipes.html
+
+import datetime as dt
+from ib_insync import *
+
+ib = IB()
+# ib.connect('127.0.0.1', 4001, clientId=102)
+ib.connect('127.0.0.1', 7496, clientId=106)
+
+# Get path of running module
+import sys, os
+# cDir = os.path.dirname(sys.argv[0])
+# cDir = cDir + '/output/'
+
+#contract = Stock('TSLA', 'SMART', 'USD')
+contract = Stock('AAPL', 'SMART', 'USD')
+
+
+# contract = Forex('EURUSD')
+# bars = ib.reqHistoricalData(
+#     contract, endDateTime='', durationStr='30 D',
+#     barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+#
+# # convert to pandas dataframe:
+# df = util.df(bars)
+# print(df)
+# sys.exit()
+
+
+
+#dt = YYYYMMDD{SPACE}hh:mm:ss[{SPACE}TMZ]
+#startdt = dt.datetime(2023, 1, 15)
+
+
+# comtract:         a qualified contract not just a ticker
+# endDateTime:      the start of the loop as it starts with current and goes back through time.
+#                   so it will get everything BEFORE that time.
+#                   Example: edt = '20221230 23:59:59' will get all bars befor the end of 2022
+#                            edt = '' will get everything before the current bar
+# duration:         the time frame reqHistoricalData will go back
+#                   without while loop, it would get the two days of 1 min bars preceding edt
+# NOTE:             duration format is integer{SPACE}unit (S|D|W|M|Y)
+# barSizeSetting:   size of each bar data to return, 1 min, 5 min, 1 hour, 1 day etc
+# whatToShow:       TRADES, MIDPOINT, BID, ASK etc
+# useRTH            True or False, show Regular Trading Hours
+# formatDate:       set to 1, but not sure what it does...
+
+
+#edt = '20000101 00:00:01'
+edt = ''
+
+barsList = []
+bars = ib.reqHistoricalData(
+    contract,
+    endDateTime=edt,
+    durationStr='30 Y',
+    barSizeSetting='1 day',
+    whatToShow='TRADES',
+    useRTH=True,
+    formatDate=1)
+
+barsList.append(bars)
+
+# save to CSV file
+allBars = [b for bars in reversed(barsList) for b in bars]
+df = util.df(allBars)
+# df.to_csv(cDir + contract.symbol + ' 1day.csv', index=False)
+print(df)
