fe4c8d75ba289ab1f56bc8c760bc72b1bd14b91c
Sundeep Chadha <sundeep_chadha@yahoo.com>
2025-04-18 12:49:22 -0500

    More updates

--- Scripts/SC_Download_YouTube_yt_dlp.py
+++ Scripts/SC_Download_YouTube_yt_dlp.py
@@ -1,14 +1,11 @@
 import yt_dlp
 
+print("The version of yt_dlp is : ", yt_dlp.version.__version__)
 
-
-# I checked it out on 4/20/2024 - it works
-url = input("Enter the video url : ")
-
+url = input("\nEnter the video url : ")
 # ydl_opts = {'format':'best[ext=mp4]'}
 ydl_opts = {}
-
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
   ydl.download([url])
 
-print ("Video Download Successful")
+print ("\nVideo Download Successful")
