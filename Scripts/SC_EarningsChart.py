b4248e8924abacc8c4ed5be6a5ec9145ddd49d02
Sundeep Chadha <sundeep_chadha@yahoo.com>
2026-07-09 14:34:46 -0500

    More updates

--- Scripts/SC_EarningsChart.py
+++ Scripts/SC_EarningsChart.py
@@ -1949,8 +1949,8 @@ for ticker_raw in ticker_list:
         logging.error("the right of the sheet in the xlsx file")
         logging.error("The fix is to delete the columns that have N/A dates in the AAII QTR_FIN.xlsx file")
         logging.error("Please delete those columns and re-run")
+        logging.error("The column number is " + str(i_itr+3) + ". The date is " + str(i))
         logging.error("===========================================================================")
-        logging.error("The column number is " + str(i_itr+3) + "The date is " + str(i))
         sys.exit(1)
       qtr_sales = row['Total Revenue']
       qtr_bv = row['Total Stockholder Equity']
